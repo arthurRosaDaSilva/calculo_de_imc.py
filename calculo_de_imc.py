b83d83277cd7c83f479da7395f85a9bb2be8c6cf
arthurRosaDaSilva <arthurrosasilva324@gmail.com>
2025-06-06 15:46:59 -0300

    fix: Utilização do Try And Except

--- calculo_de_imc.py
+++ calculo_de_imc.py
@@ -1,20 +1,28 @@
-nome = input("Digite seu nome: ")
-alt = float(input("Digite sua altura: "))
-peso = float(input("Digite seu peso: "))
+try:
+    nome = input("Digite seu nome: ")
+    alt = float(input("Digite sua altura: "))
+    peso = float(input("Digite seu peso: "))
 
-IMC = peso / (alt*alt)
+    IMC = peso / (alt*alt)
 
-if IMC <= 18.4:
-    print("Você está abaixo do peso !")
-elif IMC >=18.5 and IMC <= 24.9:
-    print("Seu peso está normal")
-elif IMC >= 25.0 and IMC <= 29.9:
-    print("Você está acima do peso !")
-elif IMC >= 30.0 and IMC <= 34.9:
-    print("Você está com Obseidade Grau I !!!")
-elif IMC >= 35.0 and IMC <= 39.9:
-    print("Você está com Obesidade Grau II !!!")
-else:
-    print("Você está com Obesidade Grau III(Mórbida) !!!")
-print("----------------------------------------------------------------------------------------------------------")
-print(f"\n{nome}, seu IMC é: {IMC:.1f}")
+    if IMC <= 18.4:
+        print("Você está abaixo do peso !")
+    elif IMC >=18.5 and IMC <= 24.9:
+        print("Seu peso está normal")
+    elif IMC >= 25.0 and IMC <= 29.9:
+        print("Você está acima do peso !")
+    elif IMC >= 30.0 and IMC <= 34.9:
+        print("Você está com Obesidade Grau I !!!")
+    elif IMC >= 35.0 and IMC <= 39.9:
+        print("Você está com Obesidade Grau II !!!")
+    else:
+        print("Você está com Obesidade Grau III(Mórbida) !!!")
+    print("----------------------------------------------------------------------------------------------------------")
+    print(f"\n{nome}, seu IMC é: {IMC:.1f}")
+
+except ValueError:
+    print("Erro: Por favor, digite valores numéricos válidos para altura e peso.")
+except ZeroDivisionError:
+    print("Erro: A altura não pode ser zero.")
+except Exception as e:
+    print(f"Ocorreu um erro inesperado: {e}")
